dd1533168d9f511675348db551c7e411cf9169c1
Priyang Pachkhede <ppachkhede@peppergroup.co.uk>
2020-04-28 09:09:24 +0100

    Test

---
+++ SCA_Northwind_trunk/Migrations/1.0.0-Baseline/002_20200428-0905_ppachkhede.sql
@@ -0,0 +1,20 @@
+﻿-- <Migration ID="d4b43c7f-8f0a-478b-ad37-e368d3b15603" />
+GO
+
+PRINT N'Altering [dbo].[Customers]'
+GO
+ALTER TABLE [dbo].[Customers] ALTER COLUMN [Address] [varchar] (100) NULL
+GO
+PRINT N'Altering [dbo].[Categories]'
+GO
+ALTER TABLE [dbo].[Categories] ADD
+[CategoryType] [nvarchar] (50) NULL,
+[MainCategory] [int] NOT NULL CONSTRAINT [DF__Categorie__MainC__75A278F5] DEFAULT ((0)),
+[testCol] [nvarchar] (10) NULL,
+[testCol1] [nvarchar] (10) NULL,
+[testCol2] [nvarchar] (10) NULL,
+[testCol3] [nvarchar] (10) NULL,
+[testCol4] [nvarchar] (10) NULL,
+[testCol5] [nvarchar] (10) NULL,
+[testCol6] [nvarchar] (10) NULL
+GO
--- SCA_Northwind_trunk/Schema-Model/dbo/Tables/Categories.sql
+++ SCA_Northwind_trunk/Schema-Model/dbo/Tables/Categories.sql
@@ -3,7 +3,16 @@ CREATE TABLE [dbo].[Categories]
 [CategoryID] [int] NOT NULL IDENTITY(1, 1),
 [CategoryName] [nvarchar] (15) NOT NULL,
 [Description] [ntext] NULL,
-[Picture] [image] NULL
+[Picture] [image] NULL,
+[CategoryType] [nvarchar] (50) NULL,
+[MainCategory] [int] NOT NULL CONSTRAINT [DF__Categorie__MainC__75A278F5] DEFAULT ((0)),
+[testCol] [nvarchar] (10) NULL,
+[testCol1] [nvarchar] (10) NULL,
+[testCol2] [nvarchar] (10) NULL,
+[testCol3] [nvarchar] (10) NULL,
+[testCol4] [nvarchar] (10) NULL,
+[testCol5] [nvarchar] (10) NULL,
+[testCol6] [nvarchar] (10) NULL
 )
 GO
 ALTER TABLE [dbo].[Categories] ADD CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED  ([CategoryID])
